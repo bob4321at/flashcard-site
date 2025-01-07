5c55f32f06074e7beefa49389c9a3e46016865cf
bob4321at <judeberin113@gmail.com>
2025-01-06 16:19:24 -0800

    asks you the question no answering

--- templates/js/index.js
+++ templates/js/index.js
@@ -4,11 +4,18 @@ async function getQuestion() {
   let test = await fetch("/question", {
     "method": "GET",
   })
-  test.text().then(function(text) {
-    let new_text = text.replace(/"/g, '')
+    let new_text = test.text()
+    new_text = (await new_text).replace(/"/g, '')
     new_text = atob(new_text)
     let object = JSON.parse(new_text)
     console.log(object)
-  })
+
+  return object
+}
+
+async function updateUi() {
+  let prompt = await getQuestion()
+
+  question.innerHTML = prompt.Question
 }
 
